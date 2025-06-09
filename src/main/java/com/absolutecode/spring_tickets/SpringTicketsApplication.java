445f927044f09eca4de469d90862dabb90faafa4
Tim Palacios <timpalacios@u.boisestate.edu>
2025-06-08 21:22:04 -0600

    Initial Commit

---
+++ src/main/java/com/absolutecode/spring_tickets/SpringTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.absolutecode.spring_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/absolutecode/spring_tickets/SpringTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absolutecode.spring_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
